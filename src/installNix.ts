3c12b372af6c0c1bd3571a053772c20ba6ae94d5
Misha Zharov <mishazharov1@gmail.com>
2022-12-30 23:36:41 -0800

    More sudo

--- src/installNix.ts
+++ src/installNix.ts
@@ -1,6 +1,6 @@
 // Based on https://github.com/cachix/install-nix-action
-import { execFileSync } from 'child_process';
+import { execSync } from 'child_process';
 
 export function run(emacsCIVersion) {
-  execFileSync(`${__dirname}/install-nix.sh`, [emacsCIVersion], { stdio: 'inherit' });
+  execSync(`sudo ${__dirname}/install-nix.sh`, [emacsCIVersion], { stdio: 'inherit' });
 }
